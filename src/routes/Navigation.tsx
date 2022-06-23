94c09fb4c2173c84eab73c074935d69fd3b6e033
Giovanni Rosales <giovanni.rosales@borngroup.com>
2022-06-23 11:58:35 -0600

    Componentes Lazy added

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route, NavLink, Navigate } from "react-router-dom";
+import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyload/pages";
 import logo from '../logo.svg';
 
 export const Navigation = () => {
@@ -9,30 +10,30 @@ export const Navigation = () => {
           <img src={logo} alt="React's logo" />
           <ul>
             <li>
-              <NavLink to="/home" className={({isActive}) => {
+              <NavLink to="/lazy1" className={({isActive}) => {
                 if(isActive) return 'nav-active';
-              }}>Home</NavLink>
+              }}>Lazy 1</NavLink>
             </li>
             <li>
-              <NavLink to="/about" className={({isActive}) => {
+              <NavLink to="/lazy2" className={({isActive}) => {
                 if(isActive) return 'nav-active';
-              }}>About</NavLink>
+              }}>Lazy 2</NavLink>
             </li>
             <li>
-              <NavLink to="/users" className={({isActive}) => {
+              <NavLink to="/lazy3" className={({isActive}) => {
                 if(isActive) return 'nav-active';
-              }}>Users</NavLink>
+              }}>Lazy 3</NavLink>
             </li>
           </ul>
         </nav>
 
         <Routes>
-          <Route path="about" element={ <h1>About Page</h1> }/>
-          <Route path="users" element={ <h1>Users Page</h1> }/>
-          <Route path="home" element={ <h1>Root Page</h1> }/>
+          <Route path="lazy1" element={ <LazyPage1 /> }/>
+          <Route path="lazy2" element={ <LazyPage2 /> }/>
+          <Route path="lazy3" element={ <LazyPage3 /> }/>
 
           {/* Not found route */}
-          <Route path="/*" element={ <Navigate to="/home" replace /> }/>
+          <Route path="/*" element={ <Navigate to="/lazy1" replace /> }/>
         </Routes>
       </div>
     </BrowserRouter>
