7794cbdbd66e5c459ad576699f28073a12ccb74d
Giovanni Rosales <giovanni.rosales@borngroup.com>
2022-06-28 11:51:15 -0600

    Control props alt

--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -6,20 +6,16 @@ export const useShoppingCart = () => {
 
   const onProductCountChange = ({ count, product }: onChangeArgs) => {
     setShoppingCart((prevState) => {
-      const actualProduct = shoppingCart[product.id] || { ...product, count: 0 }
-
-      if(Math.max(actualProduct.count + count, 0) > 0) {
-        actualProduct.count += count;
-
-        return {
-          ...prevState,
-          [product.id]: actualProduct
-        };
-      }
-
-      const { [product.id]: deletedItem, ...rest } = prevState;
+      if ( count === 0 ) {
+      const { [product.id]: itemToDelete, ...rest} = prevState;
 
       return rest;
+      }
+
+      return {
+      ...prevState,
+      [product.id]: {...product, count}
+      };
     });
   };
 
